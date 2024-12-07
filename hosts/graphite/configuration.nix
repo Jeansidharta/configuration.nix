3c4fa12db44b130e251a8e18e0715060fd4bed25
sidharta <jeansidharta@gmail.com>
2024-12-07 12:55:53 -0500

    graphite: add bluetooth support

--- hosts/graphite/configuration.nix
+++ hosts/graphite/configuration.nix
@@ -18,4 +18,7 @@
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="backlight", RUN+="${pkgs.coreutils}/bin/chgrp video $sys$devpath/brightness", RUN+="${pkgs.coreutils}/bin/chmod g+w $sys$devpath/brightness"
   '';
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  services.blueman.enable = true;
 }
