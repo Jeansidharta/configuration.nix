b23ff7adc7a7878e8082a79c99ea4cb5a70feb54
sidharta <jeansidharta@gmail.com>
2024-12-08 09:02:22 -0500

    common: add more pipewire connections

--- hosts/common/configuration.nix
+++ hosts/common/configuration.nix
@@ -49,6 +49,9 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
   };
 
   services.udev.extraRules = ''
