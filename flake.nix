6d014210d0a0a3adb851c287d32a2c6bf95dfe48
sidharta <jeansidharta@gmail.com>
2024-12-28 00:24:34 -0300

    home-manager: set to correct version

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-stable";
     theme.url = "path:./theming";
     disko = {
