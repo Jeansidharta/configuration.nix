14517f52debd408ce8ce792b9ccb1b41f59cacd8
sidharta <jeansidharta@gmail.com>
2025-03-06 10:40:00 -0500

    Update secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,12 +3,16 @@ let
     system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFEVFzY+b9v9M2zY3qOUCnsHdEcOnhERQB0jeyAkg80a";
     sidharta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF6KBaW5uNXP3Zav9MYReG37mkYB8yBU2l0RbnS6H2tT";
   };
-  obsidian.system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFUJpSJe6cceGqXjKuUiWLpYodzYKCKSihwiTIpSejW";
+  obsidian = {
+    system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGFUJpSJe6cceGqXjKuUiWLpYodzYKCKSihwiTIpSejW";
+    sidharta = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDig6qJstpy9HOVdJkvhc15ywIdRwUiH5uZ7lbwNW0rZ";
+  };
 
   allPublicKeys = [
     graphite.system
     graphite.sidharta
     obsidian.system
+    obsidian.sidharta
   ];
 in
 {
