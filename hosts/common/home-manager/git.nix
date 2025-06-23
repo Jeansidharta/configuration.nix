1aafb812a60d66ee06c1b5745f95ae92ac4747f2
sidharta <jeansidharta@gmail.com>
2025-06-23 02:38:33 -0400

    Add git aliases

--- hosts/common/home-manager/git.nix
+++ hosts/common/home-manager/git.nix
@@ -4,6 +4,14 @@
     enable = true;
     userName = "sidharta";
     userEmail = "jeansidharta@gmail.com";
+    aliases = {
+      lg = "log --graph --format=\"format:%C(auto)%h%C(reset) %C(white)-%C(reset) %C(italic blue)%ad%C(reset) %C(italic cyan)(%ar)%C(reset)%C(auto)%d%C(reset)%n %C(white)⤷%C(reset) %s %C(241)- %aN <%aE>%C(reset)%n%w(0,7,7)%+(trailers:only,unfold)\"";
+      s = "status --short";
+      a = "add .";
+      c = "commit";
+      ac = "!sh 'git add . && git commit'";
+      ca = "commit --amend";
+    };
     extraConfig = {
       init.defaultBranch = "main";
       core = {
