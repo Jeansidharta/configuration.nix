deee9774129be97558a1fcc7d462b5362921dfcf
sidharta <jeansidharta@gmail.com>
2025-08-11 12:12:30 -0400

    Add users to allowed proxyr

--- hosts/obsidian/configuration.nix
+++ hosts/obsidian/configuration.nix
@@ -26,8 +26,10 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDig6qJstpy9HOVdJkvhc15ywIdRwUiH5uZ7lbwNW0rZ jeansidharta@gmail.com"
       # My phone
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7Zp5PotpXLi0ZSby7zm1B2Ca6GyIL76Rew9zzDCTKu u0_a270@localhost"
-      # My notebook
+      # My laptop
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF6KBaW5uNXP3Zav9MYReG37mkYB8yBU2l0RbnS6H2tT sidharta@graphite"
+      # My partner's laptop
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIvVcRT7OfCgWBxvqqfw1u7xZnsrTXGaommf2m6AVlGd suzana@Nemo"
     ];
     isNormalUser = true;
     extraGroups = [ ];
