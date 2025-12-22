84ef7cbca4ae7bdae56fc73732e752d6e13fe34d
sidharta <jeansidharta@gmail.com>
2025-12-21 20:03:28 -0500

    Update graphite disko config

--- hosts/graphite/disko-config.nix
+++ hosts/graphite/disko-config.nix
@@ -1,26 +1,21 @@
-# USAGE in your configuration.nix.
-# Update devices to match your hardware.
-# {
-#  imports = [ ./disko-config.nix ];
-#  disko.devices.disk.main.device = "/dev/sda";
-# }
 {
   disko.devices = {
     disk = {
       ssd = {
         type = "disk";
-        # 128 GiB SSD
-        device = "/dev/disk/by-id/ata-SanDisk_SSD_PLUS_120_GB_180525804439";
+        device = "/dev/disk/by-id/ata-Sandisk_SSD_PLUS_1TB_2529DYD03687";
         content = {
           type = "gpt";
           partitions = {
             boot = {
               size = "1M";
+              uuid = "787e9d3f-ee33-4da3-a01d-d8c8babda2fd";
               type = "EF02"; # for grub MBR
             };
             ESP = {
               size = "1G";
               type = "EF00";
+              uuid = "12cd83a1-474c-4566-8811-659aeba4a6ed";
               content = {
                 type = "filesystem";
                 format = "vfat";
@@ -30,6 +25,7 @@
             };
             plainSwap = {
               size = "16G";
+              uuid = "0a0f2eea-2f9f-4e1e-b712-d5120d5b743c";
               content = {
                 type = "swap";
                 discardPolicy = "both";
@@ -38,10 +34,16 @@
             };
             root = {
               size = "100%";
+              uuid = "94825f69-fc2a-4cf9-b91d-c7a45bc1d7fa";
               content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
+                type = "luks";
+                name = "crypted";
+                settings.allowDiscards = true;
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
               };
             };
           };
