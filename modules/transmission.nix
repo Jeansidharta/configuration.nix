767da5bf46b2a9363734df96764459d375e6abea
j_sidharta <jeansidharta@gmail.com>
2026-06-09 00:28:42 -0300

    Update dms settings

--- modules/transmission.nix
+++ modules/transmission.nix
@@ -6,5 +6,11 @@
     enable = true;
     openFirewall = true;
     package = pkgs.transmission_4;
+    settings = {
+      rpc-bind-address = "0.0.0.0";
+      rpc-whitelist = "127.0.0.1,::1,192.168.*.*";
+      rpc-whitelist-enabled = false;
+      rpc-host-whitelist-enabled = false;
+    };
   };
 }
