1dae4a0e3aef27d525386a4daa622b73e2ec13ef
j_sidharta <jeansidharta@gmail.com>
2026-07-31 08:03:56 -0300

    Add gmail with mbsync

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -19,6 +19,7 @@ in
   "nix-github-token.age".publicKeys = allPublicKeys;
   "coffee-psk.age".publicKeys = allPublicKeys;
 
+  "gmail-imap-password.age".publicKeys = allPublicKeys;
   "weron-base-password.age".publicKeys = allPublicKeys;
   "weron-base-key.age".publicKeys = allPublicKeys;
   "wifi.age".publicKeys = allPublicKeys;
